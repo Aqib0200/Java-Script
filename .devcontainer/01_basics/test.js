c1fc6908c40568b02462e0694e898e0ae6016ea4
Muhammad Aqib <muhammadaqib0200@gmail.com>
2026-01-04 17:34:49 +0000

    let , const , var

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Aqib");
+const accountId = 14321;
+let accouuntEmail = "aqib12@gmail.com"
+var accountPassword = 7689
+accountCity="Peshawar"
+let accountState;
+
+// accountId=2  //Not Alowed 
+
+accouuntEmail= "Aqib@gmail.com"
+accountPassword= 432
+accountCity="jaipur"
+
+console.table([accouuntEmail,accountPassword,accountCity,accountState])
+
+console.log(accountId);
+
+
