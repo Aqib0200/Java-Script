47dd9361a8a9c7c465337870ae1de9edb5ac9f24
Muhammad Aqib <muhammadaqib0200@gmail.com>
2026-01-04 18:12:20 +0000

    conversion operations

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = 33
+
+// console.log(typeof score);
+
+// let newNumber=Number(score)
+// console.log(newNumber);
+
+// 33 => 33
+// 33abc => NaN
+// "Aqib" => NaN
+// true => 1 , fase=> 0
+// null => 0
+
+let isLoggedIn = "Aqib"
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+// "" => false
+// 1 => true , 0 => false
+// "Aqib" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
