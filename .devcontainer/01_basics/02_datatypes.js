567e4be709ed9ae9e641218c889cecdef8d70fbf
Muhammad Aqib <muhammadaqib0200@gmail.com>
2026-01-04 17:48:36 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as newer
+// alert(3+3) //we are using nodejs 
+
+let name = "Aqib"
+let age = 20
+let isPassed = true
+let state;
+let newstate= null
+
+// string
+// Number
+// Boolean
+// Null
+// Undefine
+// Bigint
+// Symbol
+
+
+// Object 
+
+console.log(typeof age);
+console.log(typeof null); // type of null is object
+console.log(typeof undefined);
+
+
+
